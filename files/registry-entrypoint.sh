440497ff64c55f2f195c952ee2a944b9cc3cbfb5
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2017-04-28 06:41:36 -0400

    Update environment variable names
    Looks like anything prefixed with `REGISTRY_` is automatically appended
    to the configuration; which means that base64 entries were being
    injected into the config and blowing it up.

--- files/registry-entrypoint.sh
+++ files/registry-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-cert_tarball='/tmp/registry_tls.tar.gz'
+cert_tarball='/tmp/privateregistry_tls.tar.gz'
 conf='/etc/docker/registry/config.yml'
 gen_ssl_cert='/usr/local/bin/gen-ssl-cert'
 
 # if there are entries specified in $REGISRY_AUTH_ENTRIES, drop them into
 # the auth file
-if [ ! -z "${REGISTRY_AUTH_ENTRIES}" ]; then
-  echo ${REGISTRY_AUTH_ENTRIES} | base64 -d > ${REGISTRY_AUTH_HTPASSWD_PATH}
+if [ ! -z "${PRIVATEREGISTRY_AUTH_ENTRIES}" ]; then
+  echo ${PRIVATEREGISTRY_AUTH_ENTRIES} | base64 -d > ${REGISTRY_AUTH_HTPASSWD_PATH}
 fi;
 
 # create certs
@@ -16,8 +16,8 @@ cert_root=$(dirname ${REGISTRY_HTTP_TLS_KEY})
 mkdir -p ${cert_root}
 
 # if certs are passed in via environment, write them to disk
-if [ ! -z "${REGISTRY_TLS}" ]; then
-  echo ${REGISTRY_TLS} | base64 -d > ${cert_tarball}
+if [ ! -z "${PRIVATEREGISTRY_TLS}" ]; then
+  echo ${PRIVATEREGISTRY_TLS} | base64 -d > ${cert_tarball}
 
   cd ${cert_root}
   tar xzf ${cert_tarball}
@@ -27,10 +27,10 @@ fi;
 ${gen_ssl_cert}
 
 # drop configuration in
-if [ ! -z "${REGISTRY_CONF}" ]; then
+if [ ! -z "${PRIVATEREGISTRY_CONF}" ]; then
   mkdir -p $(dirname ${conf})
 
-  echo ${REGISTRY_CONF} | base64 -d > ${conf}
+  echo ${PRIVATEREGISTRY_CONF} | base64 -d > ${conf}
 fi;
 
 exec /entrypoint.sh "$@"
